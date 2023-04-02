bd437aa1a3a25dc328243b40d50a630d0260e860
Ester N  Freitas <101005237+TherNF@users.noreply.github.com>
2023-04-02 00:53:55 -0300

    Singleton

---
+++ padroes_projeto_java/src/one/digitalinovation/gof/SingletonEager.java
@@ -0,0 +1,12 @@
+package one.digitalinovation.gof;
+
+public class SingletonEager {
+    private static SingletonEager instancia= new SingletonEager();
+    private SingletonEager() {
+        super();
+    }
+    public static SingletonEager getInstancia(){
+
+        return instancia;
+    }
+}
